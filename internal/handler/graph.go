44b0a79dec5408d783dc879368ace90a3c53c57f
Vallghall <romangusev2001@mail.ru>
2022-07-27 15:48:38 +0300

    Add extra stats return for /relations endpoint

--- internal/handler/graph.go
+++ internal/handler/graph.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/Vallghall/mt/test/internal/model/fact"
+	"github.com/Vallghall/mt/test/internal/model/graph"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"regexp"
@@ -31,9 +32,50 @@ func (h *Handler) addFact(c *gin.Context) {
 	})
 }
 
+type PersonWithRelationsCount struct {
+	fact.Person
+	Count int `json:"count"`
+}
+
+type Response struct {
+	PersonWithMaxRelations PersonWithRelationsCount `json:"person_with_max_relations"`
+	PersonWithMinRelations PersonWithRelationsCount `json:"person_with_min_relations"`
+	AverageRelationsAmount int                      `json:"average_relations_amount"`
+	graph.Graph
+}
+
 func (h *Handler) getRelationsGraph(c *gin.Context) {
 	g, err := h.s.GetGraph()
 	catch(c, err, http.StatusInternalServerError)
 
-	c.JSON(http.StatusOK, g)
+	m := make(map[fact.Person]int)
+	var r Response
+	for _, val := range g.Facts {
+		if _, ok := m[val.A]; !ok {
+			m[val.A] = 0
+		}
+		m[val.A]++
+	}
+
+	min, max, count := 999, 0, 0
+	for k, v := range m {
+		count++
+		r.AverageRelationsAmount += v
+		if v > max {
+			max = v
+			r.PersonWithMaxRelations.Person = k
+			r.PersonWithMaxRelations.Count = v
+		}
+
+		if v < min {
+			min = v
+			r.PersonWithMinRelations.Person = k
+			r.PersonWithMinRelations.Count = v
+		}
+	}
+
+	r.AverageRelationsAmount /= count
+	r.Graph = g
+
+	c.JSON(http.StatusOK, r)
 }
